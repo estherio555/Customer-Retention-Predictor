9f6a91cf21f51ed77fbbbd179e00681c6c57dc28
Esther <estherio555@yahoo.co.uk>
2025-02-05 03:11:35 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import pandas as pd
 
-from
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+# print(os.environ)
+
+import boto3
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
+
+from services_handle.logger.loggings import Logger
+
+from services_handle.get_credentials import credentials
